d5e45012ab302f37ea437ca9b4613a7c44b80f5e
Gaurav Kumar <gaurav.kumar@lapits.com>
2021-09-20 11:28:27 +0530

    changes done

--- src/helper_services/mail.service.js
+++ src/helper_services/mail.service.js
@@ -5,29 +5,27 @@ var mailer = async function(email, subject, template, options) {
     try {
         return new Promise((resolve, reject) => {
             try {
+               
                 var transporter = nodemailer.createTransport({
-                    host: process.env.MAIL_HOST,
-                    port: process.env.MAIL_PORT,
+                    service: 'gmail',
                     auth: {
-                        user: process.env.MAIL_USER,
-                        pass: process.env.MAIL_PASS
+                        user: process.env.SMTP_EMAIL,
+                        pass: process.env.SMTP_PASSWORD
                     },
-                    logger: false, // log to console
+                    logger: true, // log to console
                     debug: true // include SMTP traffic in the logs
                 });
+
                 ejs.renderFile(template, options, function(error, result) {
                     if (error) {
                         reject(error);
                     } else {
-                        transporter.sendMail({
-                            to: email,
-                            subject: subject,
-                            from: `Fancy <${process.env.MAIL_SENDER}>`,
-                            html: result
-                        }).then((info) => {
-                            console.log("message sent successfully ");
-                            resolve(info);
+                        transporter.sendMail({ to: email, subject: subject, from: process.env.SMTP_EMAIL, html: result }).then((info) => {
+                        console.log("message sent successfully ");                          
+                        resolve(info);
+                        
                         }).catch((error) => {
+                            console.log(error);
                             reject(error);
                         });
                     }
