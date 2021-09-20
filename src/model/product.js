6b069d165ca71da691e2fb240adc7dae33eb1b17
sushant gupta <sushant@lapits.com>
2021-09-20 15:15:20 +0530

    changes products route

--- src/model/product.js
+++ src/model/product.js
@@ -13,6 +13,48 @@ const productAddSchema = new mongoose.Schema({
   product_tokenType: {
     type: String,
   },
+  product_tags: [
+    {
+      type: String,
+    },
+  ],
+  product_weight: {
+    type: String,
+  },
+  product_weightUnit: {
+    type: String,
+  },
+  product_sizeDetails: [
+    {
+      color: {
+        type: String,
+      },
+      qty: {
+        type: Number,
+      },
+      image: [
+        {
+          type: String,
+        },
+      ],
+    },
+  ],
+  product_tokenDetails: [
+    {
+      token_type: {
+        type: String,
+      },
+      token_name: {
+        type: String,
+      },
+      contact_address: {
+        type: String,
+      },
+      token_id: {
+        type: String,
+      },
+    },
+  ],
 });
 
 const AddProducts = mongoose.model('Products', productAddSchema);
