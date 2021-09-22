5fc6fdb314f11fd9ced584211193249859319223
Gaurav Kumar <gaurav.kumar@lapits.com>
2021-09-22 12:45:04 +0530

    changes done

---
+++ src/services/smartContract.service.js
@@ -0,0 +1,45 @@
+const fancyContractAbi = require('../ABI/usdt-contract-abi-fancy');
+const web3 = require('web3');
+const config = require('../config/default.json');
+const web3http = new web3(new web3.providers.HttpProvider(config.HTTP_URL_INFURA));
+
+class SmartContract {
+
+    constructor() { }
+
+    // To get a new Contract Instance from Contract ABI & Contract Address
+    getContractInstance = async (contract_address) => {
+        try {
+            const contract = await new web3http.eth.Contract(fancyContractAbi, contract_address);
+            return contract;
+        } catch (error) {
+            console.log('Error in Contract Fetching: ', error);
+            throw error;
+        }
+    }
+
+
+    // To get address balance from blockchain
+    getAddressBalance = async (address, contract_address) => {
+        // console.log('address: ', address);
+        try {
+            const contract = await this.getContractInstance(contract_address);
+            console.log('contact: ', contract);
+
+            // let balance = await contract.methods.balanceOf(address).call({from: address});
+            let balance = await contract.methods.balanceOf(address).call();
+            console.log('balance: ', balance);
+            // balance = balance / Math.pow(10, config.get('usdt.DECIMAL_COUNT'));
+            // console.log('balance: ', balance);
+            return balance;
+
+        } catch (error) {
+            console.log('Error in Contract Fetching: ', error);
+            throw error;
+        }
+    }
+
+}
+
+module.exports = SmartContract;
+
