c1f4a9cd6ee81427d7935d5c04c981da0e832c62
Gaurav Kumar <gaurav.kumar@lapits.com>
2021-09-22 13:06:27 +0530

    changes done

--- src/services/smartContract.service.js
+++ src/services/smartContract.service.js
@@ -3,7 +3,7 @@ const web3 = require('web3');
 const config = require('../config/default.json');
 const web3http = new web3(new web3.providers.HttpProvider(config.HTTP_URL_INFURA));
 
-class SmartContract {
+class SmartContractService {
 
     constructor() { }
 
@@ -21,25 +21,16 @@ class SmartContract {
 
     // To get address balance from blockchain
     getAddressBalance = async (address, contract_address) => {
-        // console.log('address: ', address);
         try {
             const contract = await this.getContractInstance(contract_address);
-            console.log('contact: ', contract);
-
-            // let balance = await contract.methods.balanceOf(address).call({from: address});
             let balance = await contract.methods.balanceOf(address).call();
-            console.log('balance: ', balance);
-            // balance = balance / Math.pow(10, config.get('usdt.DECIMAL_COUNT'));
-            // console.log('balance: ', balance);
             return balance;
-
         } catch (error) {
             console.log('Error in Contract Fetching: ', error);
             throw error;
         }
     }
-
 }
 
-module.exports = SmartContract;
+module.exports = SmartContractService;
 
