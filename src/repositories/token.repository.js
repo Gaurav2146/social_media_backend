8ecbe3dc1f8ca12d543d90d81aee9a6daee43ee3
Gaurav Kumar <gaurav.kumar@lapits.com>
2021-09-27 11:40:37 +0530

    changes done

--- src/repositories/token.repository.js
+++ src/repositories/token.repository.js
@@ -3,55 +3,68 @@ const Token = require('../model/token');
 const TokenRepository = {
 
   saveToken: (tokenObj) => {
-    try {
-      return Token.create(tokenObj)
-    } catch (error) {
-      console.log(error);
-    }
+    return new Promise(async (resolve, reject) => {
+      try {
+        let token = await Token.create(tokenObj)
+        resolve(token);
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
+    })
   },
 
-  getToken: async (Symbol) => {
-    try {
-      let token = await Token.findOne({ Symbol: Symbol }, { ContractABI: 1, _id: 0 })
-      if (token) {
-        return JSON.parse(token.ContractABI);
+  getToken: (Symbol) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let token = await Token.findOne({ Symbol: Symbol }, { ContractABI: 1, _id: 0 })
+        if (token) {
+          resolve(JSON.parse(token.ContractABI));
+        }
+        else {
+          resolve('');
+        }
+      } catch (error) {
+        console.log(error);
+        reject(error);
       }
-      else {
-        return '';
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    })
   },
 
-  getAllTokens: async ({ search, index, limit }) => {
-    try {
-      let filter = {};
-      if (search) {
-        filter = { $or: [{ TokenType: { $regex: search, $options: "-i" } }, { TokenName: { $regex: search, $options: "-i" } }, { Symbol: { $regex: search, $options: "-i" } }, { ContractAddress: { $regex: search, $options: "-i" } }] }
+  getAllTokens: ({ search, index, limit }) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let filter = {};
+        if (search) {
+          filter = { $or: [{ TokenType: { $regex: search, $options: "-i" } }, { TokenName: { $regex: search, $options: "-i" } }, { Symbol: { $regex: search, $options: "-i" } }, { ContractAddress: { $regex: search, $options: "-i" } }] }
+        }
+        let total_data = await Token.find(filter).countDocuments();
+        let tokens = await Token.find(filter, { ContractABI: 0 }).skip(Number(index - 1) * Number(limit)).limit(Number(limit));
+        resolve({ tokens, total_data });
+      } catch (error) {
+        console.log(error);
+        reject(error);
       }
-      let total_data = await Token.find(filter).countDocuments();
-      let tokens = await Token.find(filter, { ContractABI: 0 }).skip(Number(index - 1) * Number(limit)).limit(Number(limit));
-      return { tokens, total_data };
-    } catch (error) {
-      console.log(error);
-    }
+    })
   },
 
-  getToken: async ({ Tokensymbol, TokenType }) => {
-    try {
-      let filter = {};
-      if (Tokensymbol) {
-        filter['Tokensymbol'] = Tokensymbol;
+  getToken: ({ Tokensymbol, TokenType }) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let filter = {};
+        if (Tokensymbol) {
+          filter['Tokensymbol'] = Tokensymbol;
+        }
+        if (TokenType) {
+          filter['TokenType'] = TokenType;
+        }
+        let token_data = await Token.findOne(filter);
+        resolve(token_data);
+      } catch (error) {
+        console.log(error);
+        reject(error);
       }
-      if (TokenType) {
-        filter['TokenType'] = TokenType;
-      }
-      let token_data = await Token.findOne(filter);
-      return token_data;
-    } catch (error) {
-      console.log(error);
-    }
+    })
   }
 };
 
