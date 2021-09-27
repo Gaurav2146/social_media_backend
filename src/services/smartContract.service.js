dc662780d0f24ca577608b0f48984b1c7afc8251
Gaurav Kumar <gaurav.kumar@lapits.com>
2021-09-27 12:45:08 +0530

    changes done

--- src/services/smartContract.service.js
+++ src/services/smartContract.service.js
@@ -8,27 +8,25 @@ class SmartContractService {
 
     constructor() {
         this.tokenRepository = tokenRepository;
-     }
+    }
 
     // To get a new Contract Instance from Contract ABI & Contract Address
     getContractInstance = async (contract_address) => {
         try {
-            let Contract_ABI = await this.tokenRepository.getToken({ Tokensymbol : 'BAYC' });
-            if( Contract_ABI &&   Contract_ABI[0] && Contract_ABI[0].ContractABI )
-            {
-                console.log(Contract_ABI , 'Contract_ABI');
-                const contract = await new web3http.eth.Contract( JSON.parse( JSON.parse(Contract_ABI[0].ContractABI) ) , contract_address);
+            let Contract_ABI = await this.tokenRepository.getToken({ Tokensymbol: 'BAYC' });
+            if (Contract_ABI && Contract_ABI[0] && Contract_ABI[0].ContractABI) {
+                console.log(Contract_ABI, 'Contract_ABI');
+                const contract = await new web3http.eth.Contract(JSON.parse(JSON.parse(Contract_ABI[0].ContractABI)), contract_address);
                 return contract;
             }
-            else
-            {
-              return '';
-            }           
+            else {
+                return '';
+            }
         } catch (error) {
             console.log('Error in Contract Fetching: ', error);
             throw error;
         }
-    } 
+    }
 
 
     // To get address balance from blockchain
