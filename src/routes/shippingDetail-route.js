10e2a9c796dac0f03db2a180ad370f302e41ee2b
Gaurav Kumar <gaurav.kumar@lapits.com>
2021-10-05 18:05:51 +0530

    changes done

--- src/routes/shippingDetail-route.js
+++ src/routes/shippingDetail-route.js
@@ -11,12 +11,12 @@ let auth = app.use(jwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'], 
         console.log(req.user, 'inside jwt');
         next();
     });
-
+       
 
 /**
  * get Address Balance from Blockchain
  * @route POST /smartContract/addShippingDetail
- * @group Upload API - Endpoints to get Address Balance
+ * @group Upload API - Endpoints to get Address Balance 
  * @returns {array of object} 200  
  * @returns {Error}  default - Unexpected error
  */
