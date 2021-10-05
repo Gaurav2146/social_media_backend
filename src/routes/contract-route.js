943cb2cb59e611f2e7a93efcc10bbd0d440956c3
Gaurav Kumar <gaurav.kumar@lapits.com>
2021-10-05 12:14:20 +0530

    changes done

--- src/routes/contract-route.js
+++ src/routes/contract-route.js
@@ -1,16 +1,7 @@
 const express = require('express');
-var jwt = require('express-jwt');
 const router = express.Router();
 const contractMetadataController = require('../controllers/contractMetadataController');
-var app = express();
 
-
-
-let auth = app.use(jwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'], userProperty: 'user' }),
-    async function (req, res, next) {
-        console.log(req.user, 'inside jwt');
-        next();
-    });
 
 /**
  * get contract Metadata from Blockchain
