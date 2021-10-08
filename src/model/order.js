b19fc5d541685d73087b4371bc6aac092e311410
Gaurav Kumar <gaurav.kumar@lapits.com>
2021-10-08 11:39:54 +0530

    changes done

---
+++ src/model/order.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+
+  product_ID:{
+    type: String,
+    required: true,       
+    trim: true
+  },
+  Wallet_ID:{
+    type: String,
+    required: true,       
+    trim: true
+  },
+  product_price: {
+    type: Number,
+    required: true,
+  },
+  product_color: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  product_size : {
+    type: String,
+    required: true,
+    trim: true
+  },
+  product_quantity : {
+    type: Number,
+    required: true,
+  },
+  Product_image: {
+    type: Array,
+    required: true,
+  },
+  nft_image: {
+    type: Object,
+    required: true,
+  },
+  shipping_Detail_Id:{
+    type: mongoose.Types.ObjectId
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
