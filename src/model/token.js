3a476531b354165011b5d594810409b0457346f7
Gaurav Kumar <gaurav.kumar@lapits.com>
2021-10-18 13:08:34 +0530

    changes done

--- src/model/token.js
+++ src/model/token.js
@@ -5,33 +5,40 @@ const tokenSchema = new mongoose.Schema({
 
     TokenType: {
         type: String,
-        require: true
+        require: true,
+        trim: true
     },
     TokenName: {
         type: String,
-        require: true
+        require: true,
+        trim: true
     },
     Symbol: {
         type: String,
         require: true,
-        unique : true
+        unique : true,
+        trim: true
     },
     ContractAddress: {
         type: String,
         require: true,
-        unique : true
+        unique : true,
+        trim: true
     },
     ContractABI: {
         type: String,
-        require: true
+        require: true,
+        trim: true
     },
     createdAt: {
         type: Date,
         default: Date.now,
+        trim: true
     },
     updatedAt: {
         type: Date,
         default: Date.now,
+        trim: true
     },
 });
 
