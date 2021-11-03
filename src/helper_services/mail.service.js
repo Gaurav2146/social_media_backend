30f8b72745156f15ac319ef25453c56bc149af59
sushant gupta <sushant@lapits.com>
2021-11-03 12:28:41 +0530

    changes google

--- src/helper_services/mail.service.js
+++ src/helper_services/mail.service.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+
 const ejs = require('ejs');
 
 const mailer = async function (email, subject, template, options) {
@@ -7,27 +8,40 @@ const mailer = async function (email, subject, template, options) {
       try {
         const transporter = nodemailer.createTransport({
           service: 'gmail',
+          pool: true,
+          host: process.env.SMTP_HOST,
+          port: process.env.SMTP_PORT,
           auth: {
             user: process.env.SMTP_EMAIL,
             pass: process.env.SMTP_PASSWORD,
           },
+          tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: true,
+          },
+          secure: false,
           logger: true, // log to console
           debug: true, // include SMTP traffic in the logs
         });
 
         ejs.renderFile(template, options, (error, result) => {
           if (error) {
-            console.log(error);
+            console.log('errorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', error);
             reject(error);
           } else {
             transporter
-              .sendMail({ to: email, subject: subject, from: process.env.SMTP_EMAIL, html: result })
+              .sendMail({
+                to: email,
+                subject: subject,
+                from: process.env.SMTP_EMAIL,
+                html: result,
+              })
               .then((info) => {
                 console.log('message sent successfully');
                 resolve('Password Reset Link Sent To Your Email');
               })
               .catch((error) => {
-                console.log(error);
+                console.log('errorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', error);
                 reject(error);
               });
           }
